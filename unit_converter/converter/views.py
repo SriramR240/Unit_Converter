5af132afcad6792d5da1146e62f61ee194d50cca
myname <rkvsriram24@gmail.com>
2025-01-09 20:58:26 +0530

    Django home setup

--- unit_converter/converter/views.py
+++ unit_converter/converter/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'home.html',{'name':'Sriram'})
+
+
