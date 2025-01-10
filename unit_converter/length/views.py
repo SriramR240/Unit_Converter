0c74aec4e4e3c08979413f86db38776d1d7237e5
myname <rkvsriram24@gmail.com>
2025-01-10 18:55:04 +0530

    added gitignore file

--- unit_converter/length/views.py
+++ unit_converter/length/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html',{'name':'Sriram'})
+    return render(request,'length.html',{'name':'Sriram'})
 
 
